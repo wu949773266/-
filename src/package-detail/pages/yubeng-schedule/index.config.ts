542bf992808572c368ff162bbcf0a3c8530ff1f6
949773266 <4091742750253177-949773266@noreply.coze.cn>
2026-04-08 16:52:07 +0800

    fix: 启用雨崩行程页的分享功能

--- src/package-detail/pages/yubeng-schedule/index.config.ts
+++ src/package-detail/pages/yubeng-schedule/index.config.ts
@@ -1,3 +1,11 @@
 export default typeof definePageConfig === 'function'
-  ? definePageConfig({ navigationBarTitleText: '雨崩徒步摄影4日游' })
-  : { navigationBarTitleText: '雨崩徒步摄影4日游' }
+  ? definePageConfig({
+      navigationBarTitleText: '雨崩徒步摄影4日游',
+      enableShareAppMessage: true,
+      enableShareTimeline: true
+    })
+  : {
+      navigationBarTitleText: '雨崩徒步摄影4日游',
+      enableShareAppMessage: true,
+      enableShareTimeline: true
+    }
